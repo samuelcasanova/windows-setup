52aa8e3dbd16b1644bfbf709577ee43f5ffa1838
Samuel <samuel.casanova@gmail.com>
2024-09-22 13:56:17 +0200

    add notepad++ and obsidian config

---
+++ install-software-scripts/21-obsidian.ps1
@@ -0,0 +1,2 @@
+Read-Host "Press enter to open a browser with the Obsidian downloads page. Download and install the latest version with default options"
+Start-Process "https://obsidian.md/download"
