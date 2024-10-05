5e203f68c6da1a19c498367a7c84fb8c4a933efe
Samuel <samuel.casanova@gmail.com>
2024-10-05 19:12:08 +0200

    set alex folders attempt

--- install-software-scripts/10-install-all-soft-from-admin-cmd.bat
+++ install-software-scripts/10-install-all-soft-from-admin-cmd.bat
@@ -1,15 +1,15 @@
-winget install "Google Chrome" --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
-winget install Obsidian --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
-winget install --id Microsoft.VisualStudioCode --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
-winget install "Microsoft Powertoys" --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
-winget install "7-Zip" --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
-winget install --id Microsoft.Office --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
-winget install --id Adobe.Acrobat.Reader.64-bit --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
-winget install Irfanview64 --silent --accept-package-agreements --accept-source-agreements --uninstall-previous --source winget
-winget install KeePassXC --silent --accept-package-agreements --accept-source-agreements --uninstall-previous --source winget
-winget install paint.net --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
-winget install --id VideoLAN.VLC --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
-winget install windirstat --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
-winget install --id Ditto.Ditto --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
-winget install --id calibre.calibre --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
-winget install --id th-ch.YouTubeMusic --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
+winget install --id Google.Chrome --scope machine --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
+winget install Obsidian --scope machine --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
+winget install --id Microsoft.VisualStudioCode --scope machine --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
+winget install "Microsoft Powertoys" --scope machine --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
+winget install "7-Zip" --scope machine --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
+winget install --id Microsoft.Office --scope machine --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
+winget install --id Adobe.Acrobat.Reader.64-bit --scope machine --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
+winget install Irfanview64 --scope machine --silent --accept-package-agreements --accept-source-agreements --uninstall-previous --source winget
+winget install KeePassXC --scope machine --silent --accept-package-agreements --accept-source-agreements --uninstall-previous --source winget
+winget install paint.net --scope machine --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
+winget install --id VideoLAN.VLC --scope machine --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
+winget install windirstat --scope machine --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
+winget install --id Ditto.Ditto --scope machine --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
+winget install --id calibre.calibre --scope machine --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
+winget install --id th-ch.YouTubeMusic --scope machine --silent --accept-package-agreements --accept-source-agreements --uninstall-previous
---
+++ install-software-scripts/41-set-alex-folders.ps1
@@ -0,0 +1,12 @@
+﻿. ./support-files/utils.ps1
+
+$o = new-object -com shell.application
+$o.Namespace('C:\Users\alexc\OneDrive\Samuel\Ocio personal\Proyectos Personales').Self.InvokeVerb("pintohome")
+$o.Namespace('C:\Users\samue\OneDrive\Peques').Self.InvokeVerb("pintohome")
+$o.Namespace('C:\Users\samue\OneDrive\Samuel\Desarrollo personal').Self.InvokeVerb("pintohome")
+$o.Namespace('C:\Users\samue\OneDrive\Samuel\Ocio personal').Self.InvokeVerb("pintohome")
+
+Set-KnownFolderPath -KnownFolder 'Documents' -Path 'C:\Users\alexc\OneDrive\Documentos'
+Set-KnownFolderPath -KnownFolder 'Pictures' -Path 'C:\Users\alexc\OneDrive\Imágenes'
+Set-KnownFolderPath -KnownFolder 'Music' -Path 'C:\Users\alexc\OneDrive\Música'
+Set-KnownFolderPath -KnownFolder 'Videos' -Path 'C:\Users\alexc\OneDrive\Vídeos'
